f9ef7aaf8f206b2bf43020e29f8f58a244453ac5
ludovic <ludovic.angenard@orange.fr>
2021-11-24 11:15:36 +0100

    beagin to handl request in the back

--- back/server.js
+++ back/server.js
@@ -4,11 +4,30 @@ const http = require('http');
 
 // Create an instance of the http server to handle HTTP requests
 let app = http.createServer((req, res) => {
+    var url = new URL (req.url, `http://${req.headers.host}`); //Récupération URL
+    let contenu = ''
     // Set a response type of plain text for the response
     res.writeHead(200, {'Content-Type': 'text/plain'});
+    switch(req.method) {
+        case 'GET':
+        break;
+        case 'POST':
+            if (url.pathname == "/addUser/"){
 
-    // Send back a response and end the connection
-    res.end('Hello World!\n');
+            }
+            if (url.pathname == "/addMessage/"){
+
+            }
+        break;
+        case 'PUT':
+            if (url.pathname == "/updateUser/"){
+
+            }
+        break;
+        case 'DELETE':
+        break;
+    }
+    res.end(contenu)
 });
 
 // Start the server on port 3000
